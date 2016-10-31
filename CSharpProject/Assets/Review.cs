6e64c62a4cd75d27a943145fcbe61cb1871dfd75
Martin <133900029@qq.com>
2016-10-31 23:56:14 +0800

    Review

---
+++ CSharpProject/Assets/Review.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Review : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
+
+
+
+
+
+
+
+// 
+// 
+// 公共语言运行时Common Language runtime//管理代码，保证正确
+// 通用类型系统  Common Type System//通过装箱和拆箱实现无缝互操作
+// 公共语言规范  Common Language Specification//最低标准
+// 
+// const int value = 10;
+// 
+// public
+// internal只在当前项目使用
+// abstract
+// sealed不能被继承
+// 
+// 静态构造函数只执行一次
+// 
+// 索引器
+// class Person
+// {
+//    private int[] intArray = new int[10];
+//    public int this[int index]
+//     {
+// 	get set
+//     }
+// }
+// 
+// 类是引用类型
+// 结构体是值类型，可以不用new
+// 
+// 封装：把客观事物封成类，并把内部实现隐藏，保证数据完整性
+// 继承：复用父类代码
+// 多态：将子类赋值给父类的能力
+// 
+// virtual
+// abstract
+// override
+// base
+// new
+// 
+// 类和类是属于的关系
+// 类和接口是can do的关系
+// 类不能多继承，接口可以多继承
+// 
+// 委托
+// public delegate void Write(string name)
+// public Wrtie write
+// 事件
+// public delegate void Write(string name)
+// public event Write write
+// C#自带
+// public delegate void EventHandler(object sender,EventArgs e)
+// MarryEventArgs : EventArgs  string message
+// 
+// 委托能够直接使用，能够类外使用
+// 事件不能直接使用，不能类外使用
+// 
+// 事件是一个特殊的委托
+
+
+
+
+
+
+
+
+
+
+
